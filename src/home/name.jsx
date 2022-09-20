ba2171ada70fa1aeddbfc381ffeffb7dc2d71eb8
Amukh1 <85003861+amukh1@users.noreply.github.com>
2022-09-19 22:31:50 -0400

    Deploy: #3

--- src/home/name.jsx
+++ src/home/name.jsx
@@ -1,14 +1,32 @@
 import '../assets/App.css'
 import { useSpring, animated, config } from "react-spring";
-import react, { useState, useRef } from "react";
+import react, { useState, useRef, useEffect} from "react";
+import {useLocation} from 'react-router-dom'
 
 export default function Name() {
-  let [name, setName] = useState('register')
+  const location = useLocation();
+  let [name, setName] = useState('')
+
+  useEffect(() => {
+  if((window.localStorage.getItem('signedIn') == 'true') && !window.location.hash.startsWith('#/dashboard')){
+    setName('dashboard')
+    console.log('dashboard')
+  }else if(window.location.hash.startsWith('#/dashboard') || window.location.hash.startsWith('#/admin')){
+    setName('logout')
+    console.log('logout')
+  }else {
+    setName('register')
+    console.log('register')
+  }
+},[location])
   return (
     <main>
       <div className="name">
        <a href="/" className='named'>Dead-Dev Studios</a>
-      <button className="register" onClick={window.location.hash.startsWith('#/dashboard') ? logout :register}>{window.location.hash.startsWith('#/dashboard') ? 'logout' :'Register'}</button>
+       <div>
+       <button className="register" onClick={admin}>Admin</button>
+      <button className="register" onClick={handle}>{name}</button>
+      </div>
       </div>
     </main>
   )
@@ -23,4 +41,18 @@ function logout() {
   console.log('logging out')
   window.localStorage.setItem('signedIn', false)
   window.location.href = '#/'
+}
+
+function admin(){
+  window.location.href = '#/admin'
+}
+
+function handle(){
+  if(window.location.hash.startsWith('#/dashboard')){
+    logout()
+  }else if(window.localStorage.getItem('signedIn') == 'true'){
+    window.location.href = '#/dashboard/user'
+  }else{
+    register()
+  }
 }
